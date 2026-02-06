05efeaf080a0a64df6fafe655488defd5613b2d7
Lecaudey <amandine.lecaudey-lecoeur@isen-ouest.yncrea.fr>
2026-02-06 09:30:17 +0100

    Gestion du site et modification de l'architechture

--- app/main.py
+++ app/main.py
@@ -1,16 +1,17 @@
-from pathlib import Path
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-BASE = Path(__file__).resolve().parent.parent
-STATIC_DIR = BASE / "public"
-TEMPLATES_DIR = Path(__file__).resolve().parent / "templates"
 
-app = FastAPI()
-app.mount("/static", StaticFiles(directory=str(STATIC_DIR)), name="public")
-templates = Jinja2Templates(directory=str(TEMPLATES_DIR))
+app = FastAPI() #app est un objet de la classe FastAPI
+
+
+
+app.mount("/static", StaticFiles(directory="public"), name="public")
+
+templates = Jinja2Templates(directory="app/templates")
+
 
 @app.get("/")
 async def root(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+    return templates.TemplateResponse(request, "home.html")
