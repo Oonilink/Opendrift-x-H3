2ee2a0a39090dc8cea33c4860dc8f156ec5d29ff
Lecaudey <amandine.lecaudey-lecoeur@isen-ouest.yncrea.fr>
2026-02-23 14:08:31 +0100

    On avait bien la simulation (correcte) qui s'executait et se stockait au bon endroit, mais l'affichage retour n'était pas opérationnel. Le problème a été résolu en corrigeant l'appel à la fonction dans la pipeline, les routes fastAPI ainsi que le html.

--- simulations/plotting/carte_interactive.py
+++ simulations/plotting/carte_interactive.py
@@ -15,11 +15,13 @@ from h3.api.basic_int import latlng_to_cell, cell_to_boundary, cell_to_latlng
 import xarray as xr
 
 
-def create_map(fichier_nc):
+def create_map(fichier_nc, map_path):
     """
     Docstring pour create_map
     
     :param fichier_nc: fichier NetCDF contenant les résultats de la simulation
+    :param map_path: chemin de sauvegarde de la carte HTML générée
+    :return: None (la carte est sauvegardée à map_path)
 
     """
     ######### LECTURE DU NETCDF (aucune simulation exécutée) ###########
@@ -136,36 +138,9 @@ def create_map(fichier_nc):
         time_slider_drag_update=True,
     ).add_to(m)
 
-    """
-
-    for t in range(nb_epoque - 1):
-        for i in range(nb_trajectoire):   # pour chaque particule
-            la = np.asarray(lat[i, t]).item()
-            lo = np.asarray(lon[i, t]).item()
-
-        #vérif si nan
-            if np.isnan(la) or np.isnan(lo):
-                continue
-
-            # Convertit en H3
-            h = latlng_to_cell(la, lo, resolution)
-
-            # Récupère les coordonnées de la cellule H3
-            boundary = cell_to_boundary(h)
-            boundary = [(b_lat, b_lon) for (b_lat, b_lon) in boundary]
-            boundary.append(boundary[0])  # ferme le polygone
-
-            # Ajout à la carte
-            folium.Polygon(
-                locations=boundary,
-                color="blue",
-                weight=2,
-                fill=True,
-                fill_opacity=0.4
-            ).add_to(m)
-    """
+    
     # Sauvegarde carte
-    m.save("test_carte/carte_h3_2.html")
+    m.save(map_path)
 
 
 
